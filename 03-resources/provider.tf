271bd8c9fa711a0499b10717d5e2c956b50ce13c
Shiva <l.shivaprasad.160@gmailcom>
2022-01-13 21:21:55 +0530

    provi

--- 03-resources/provider.tf
+++ 03-resources/provider.tf
@@ -2,4 +2,13 @@ provider "aws" {
   region = "us-east-1"
 }
 
+terraform {
+  backend "s3" {
+    bucket = "terraform-b61"
+    key    = "sample/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+
 
